e8245fd4e7769cc9b4ed1b304c76d3f5e96ed95e
subhy <subhy0422@gmail.com>
2021-01-25 23:21:34 +0530

    Create camera capture

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const appSlice = createSlice({
-  name: 'counter',
+  name: 'app',
   initialState: {
     value: 0,
   },
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 const firebaseConfig = {
     apiKey: "AIzaSyCATX0f45TKOzHBAtjg9kZnY5nB_F4l_0s",
     authDomain: "snapchat-clone-26c18.firebaseapp.com",
@@ -6,3 +7,11 @@ const firebaseConfig = {
     messagingSenderId: "602639844123",
     appId: "1:602639844123:web:aaac0678992ebaf1bb09be"
 };
+
+const firebaseApp=firebase.initializeApp(firebaseConfig);
+const  db=firebaseApp.firestore();
+const auth=firebase.auth();
+const storage=firebase.storage();
+const provider=new firebase.auth.GoogleAuthProvider();
+
+export {db,storage,auth,provider};
