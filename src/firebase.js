cac4872c976d37f4c7053644d716c3d46d32540d
subhy <subhy0422@gmail.com>
2021-01-25 21:41:58 +0530

    Config file structure & firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCATX0f45TKOzHBAtjg9kZnY5nB_F4l_0s",
+    authDomain: "snapchat-clone-26c18.firebaseapp.com",
+    projectId: "snapchat-clone-26c18",
+    storageBucket: "snapchat-clone-26c18.appspot.com",
+    messagingSenderId: "602639844123",
+    appId: "1:602639844123:web:aaac0678992ebaf1bb09be"
+};
